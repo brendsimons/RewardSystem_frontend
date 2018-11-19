77b7726af11748dd7b4d1a8066ec2415baa3ae6a
Jari Mathe <jari.mathe@hotmail.com>
2018-11-19 16:23:43 +0100

    werk aan shop

--- src/app/shop/shop.component.ts
+++ src/app/shop/shop.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-shop',
   templateUrl: './shop.component.html',
   styles: []
 })
-export class ShopComponent implements OnInit {
+export class ShopComponent 
+ {
 
   constructor() { }
 
-  ngOnInit() {
-  }
 
 }
