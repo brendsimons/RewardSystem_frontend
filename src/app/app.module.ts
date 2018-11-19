7eaaae3fb6672d858c0bdaf65ba4dcffdee54af1
AndreasAerts <r0658500@student.thomasmore.be>
2018-11-19 10:38:25 +0100

    Angular set-up

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import {NgBootstrapModule} from "./sharedModules/ng-bootstrap.module";
+import { NavbarComponent } from './navbar/navbar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+      NgBootstrapModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
