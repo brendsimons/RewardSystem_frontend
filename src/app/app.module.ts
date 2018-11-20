adb131b1174fbc3fec50f04d2d7b0c3f01efd37c
Brend Simons <brendsimons@gmail.com>
2018-11-20 09:36:48 +0100

    Merge fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-<<<<<<< HEAD
 
 import { JwtModule } from '@auth0/angular-jwt';
 
@@ -10,14 +9,6 @@ import { NgBootstrapModule } from "./sharedModules/ng-bootstrap.module";
 import { NavbarComponent } from './navbar/navbar.component';
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { MaterialModule } from "../material-module";
-=======
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import {NgBootstrapModule} from './sharedModules/ng-bootstrap.module';
-import { NavbarComponent } from './navbar/navbar.component';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MaterialModule} from '../material-module';
->>>>>>> 91bf19b1c6c27727ab6c3e486eababab5a7e95a7
 import { DashboardModule } from './dashboard/dashboard.module';
 import { ShopModule } from './shop/shop.module';
 import { OpdrachtenIngevenModule } from './opdrachten-ingeven/opdrachten-ingeven.module';
