dbef5cec191dfd98b352036bb53d376c2624b7b4
Brend Simons <brendsimons@gmail.com>
2018-11-20 09:31:51 +0100

    Authentication partial added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { JwtModule } from '@auth0/angular-jwt';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {NgBootstrapModule} from "./sharedModules/ng-bootstrap.module";
+import { NgBootstrapModule } from "./sharedModules/ng-bootstrap.module";
 import { NavbarComponent } from './navbar/navbar.component';
-import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
-import {MaterialModule} from "../material-module";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MaterialModule } from "../material-module";
 import { DashboardModule } from './dashboard/dashboard.module';
 import { ShopModule } from './shop/shop.module';
 import { OpdrachtenIngevenModule } from './opdrachten-ingeven/opdrachten-ingeven.module';
 import { RewardsIngevenModule } from './rewards-ingeven/rewards-ingeven.module';
+
+export function jwtTokenGetter() {
+  return localStorage.getItem('access_token');
+}
 
 @NgModule({
   declarations: [
@@ -20,13 +26,26 @@ import { RewardsIngevenModule } from './rewards-ingeven/rewards-ingeven.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-      NgBootstrapModule,
-      BrowserAnimationsModule,
-      MaterialModule,
-      DashboardModule,
-      ShopModule,
-      OpdrachtenIngevenModule,
-      RewardsIngevenModule
+    NgBootstrapModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    DashboardModule,
+    ShopModule,
+    OpdrachtenIngevenModule,
+    RewardsIngevenModule,
+
+    JwtModule.forRoot({
+      config: {
+        tokenGetter: jwtTokenGetter,
+        whitelistedDomains: [
+          'https://vpn.brendsimons.be'
+        ],
+        blacklistedRoutes: [
+          'https://vpn.brendsimons.be/users/login',
+          'https://vpn.brendsimons.be/users/register',
+        ]
+      }
+    })
   ],
 
   providers: [],
