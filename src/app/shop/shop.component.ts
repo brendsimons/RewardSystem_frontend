1d72feafd103623278a12dede2ca93d2f86c0655
Jari Mathe <jari.mathe@hotmail.com>
2018-11-20 09:09:28 +0100

    theme colors in scss

--- src/app/shop/shop.component.ts
+++ src/app/shop/shop.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-shop',
   templateUrl: './shop.component.html',
-  styles: []
+  styleUrls: ['./shop.component.scss'],
 })
 export class ShopComponent 
  {
