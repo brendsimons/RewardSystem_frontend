519f6a8408fa126d8de57a8a4fa236331f0b99c5
Brend Simons <brendsimons@gmail.com>
2018-11-20 12:34:54 +0100

    jwt interceptor fix

--- src/app/rewards-ingeven/rewards-ingeven.component.ts
+++ src/app/rewards-ingeven/rewards-ingeven.component.ts
@@ -1,33 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import {Router} from '@angular/router';
-import {APIService} from '../services/api.service';
-import {Observable} from 'rxjs';
-import {map} from 'rxjs/operators';
+import { Router } from '@angular/router';
+import { APIService } from '../services/api.service';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
-  selector: 'app-rewards-ingeven',
-  templateUrl: './rewards-ingeven.component.html',
-  styleUrls: [
-      './rewards-ingeven.component.scss'
-  ]
+    selector: 'app-rewards-ingeven',
+    templateUrl: './rewards-ingeven.component.html',
+    styleUrls: [
+        './rewards-ingeven.component.scss'
+    ]
 })
 export class RewardsIngevenComponent implements OnInit {
-  public name;
-  public score;
-  public error;
+    public name;
+    public score;
+    public error;
 
-  constructor(private http: HttpClient, private api: APIService , private router: Router) { }
+    constructor(private http: HttpClient, private api: APIService, private router: Router) { }
 
-  public submit() {
-      return this.http.post(this.api.getUrl('/rewards'), { name: this.name, score: this.score })
-          .subscribe(
-              result => this.router.navigate(['dashboard']),
-              err => this.error = 'Could not authenticate'
-          );
-  }
+    public submit() {
+        this.http.post(this.api.getUrl('/rewards'), { name: this.name, score: this.score })
+            .subscribe(
+                result => this.router.navigate(['dashboard']),
+                err => this.error = 'Could not authenticate'
+            );
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
 }
--- src/app/services/jwt.interceptor.ts
+++ src/app/services/jwt.interceptor.ts
@@ -7,8 +7,10 @@ export class JwtInterceptor implements HttpInterceptor {
 
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         // add authorization header with jwt token if available
-        let access_token = JSON.parse(localStorage.getItem('access_token'));
-        if (access_token) {
+        console.log("oke");
+
+        let access_token = localStorage.getItem('access_token');
+        if (access_token != null) {
             request = request.clone({
                 setHeaders: {
                     Authorization: `Bearer ${access_token}`
