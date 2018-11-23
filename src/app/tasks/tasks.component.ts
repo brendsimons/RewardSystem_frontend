2b8a11051cf0251de41606fbec8e008bc4b994fe
Brend Simons <brendsimons@gmail.com>
2018-11-23 11:35:33 +0100

    Tasks fix

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -3,7 +3,8 @@ import { HttpClient } from '@angular/common/http';
 import { APIService } from '../services/api.service';
 import { Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
-
+import { first } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 @Component({
     selector: 'app-tasks',
@@ -13,9 +14,17 @@ import { AuthService } from '../services/auth.service';
 export class TasksComponent implements OnInit {
     tasks$: any;
 
+    public opdracht: string;
+    public punten: string;
+
     displayedColumns: string[] = ['naam', 'credits'];
 
     constructor(private http: HttpClient, private api: APIService, private auth: AuthService) { }
+
+    public submit() {
+
+
+    }
 
     ngOnInit() {
         // CHECK OP PERMISSION "ADMIN3"
--- src/app/tasks/tasks.module.ts
+++ src/app/tasks/tasks.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TasksComponent } from './tasks.component';
 import { MaterialModule } from '../../material-module';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
     imports: [
         CommonModule,
-        MaterialModule
+        MaterialModule,
+        FormsModule
     ],
     declarations: [TasksComponent]
 })
