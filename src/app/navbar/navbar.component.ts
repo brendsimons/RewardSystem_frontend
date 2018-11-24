2c97a2c0cebbe3e8d4f5bcb8bff18f9870c6d2c1
AndreasAerts <r0658500@student.thomasmore.be>
2018-11-24 19:37:42 +0100

    Dashboard pagina af en mogelijkheid om tussen donker en licht thema te kiezen

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,7 +1,9 @@
-import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import {ChangeDetectorRef, Component, HostBinding, OnInit} from '@angular/core';
 import { MediaMatcher } from "@angular/cdk/layout";
 import { AuthService } from '../services/auth.service';
 import { Router } from '@angular/router';
+import {APIService} from "../services/api.service";
+import {HttpClient} from "@angular/common/http";
 
 @Component({
     selector: 'app-navbar',
@@ -11,9 +13,12 @@ import { Router } from '@angular/router';
 export class NavbarComponent implements OnInit {
     isCollapsed = true;
     mobileQuery: MediaQueryList;
+    @HostBinding('class.indigo-pink') lightTheme: boolean = true;
+    @HostBinding('class.indigo-pink-dark') darkTheme: boolean = false;
+    theme: string = 'Donker';
 
     private _mobileQueryListener: () => void;
-    constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher, private auth: AuthService, private router: Router) {
+    constructor(private http: HttpClient, changeDetectorRef: ChangeDetectorRef, media: MediaMatcher, private api: APIService, private auth: AuthService, private router: Router) {
         this.mobileQuery = media.matchMedia('(max-width: 600px)');
         this._mobileQueryListener = () => changeDetectorRef.detectChanges();
         this.mobileQuery.addListener(this._mobileQueryListener);
@@ -21,6 +26,19 @@ export class NavbarComponent implements OnInit {
 
     ngOnDestroy(): void {
         this.mobileQuery.removeListener(this._mobileQueryListener);
+    }
+
+    toggleTheme(): void {
+
+        if (this.theme === 'Donker') {
+            this.lightTheme = false;
+            this.darkTheme = true;
+            this.theme = 'Licht';
+        } else {
+            this.darkTheme = false;
+            this.lightTheme = true;
+            this.theme = 'Donker';
+        }
     }
 
     ngOnInit() {
