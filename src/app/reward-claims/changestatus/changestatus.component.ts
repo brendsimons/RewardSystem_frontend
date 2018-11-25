2a6addf43f607522c41a1954e99bbfa4bd97efca
Brend Simons <brendsimons@gmail.com>
2018-11-25 12:55:56 +0100

    Changestatus component little fix

--- src/app/reward-claims/changestatus/changestatus.component.ts
+++ src/app/reward-claims/changestatus/changestatus.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import {ActivatedRoute, Router} from '@angular/router';
-import {RewardClaimService} from '../../services/reward-claim.service';
-import {Observable} from 'rxjs';
-import {HttpClient} from '@angular/common/http';
-import {APIService} from '../../services/api.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { RewardClaimService } from '../../services/reward-claim.service';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { APIService } from '../../services/api.service';
 
 export interface Status {
     value: string;
@@ -11,9 +11,9 @@ export interface Status {
 }
 
 @Component({
-  selector: 'app-changestatus',
-  templateUrl: './changestatus.component.html',
-  styleUrls: ['./changestatus.component.scss']
+    selector: 'app-changestatus',
+    templateUrl: './changestatus.component.html',
+    styleUrls: ['./changestatus.component.scss']
 })
 export class ChangestatusComponent implements OnInit {
     selectedValue: string;
@@ -22,23 +22,23 @@ export class ChangestatusComponent implements OnInit {
 
     constructor(private router: Router, private route: ActivatedRoute, private rewardClaimService: RewardClaimService, private http: HttpClient, private api: APIService) { }
 
-  ngOnInit() {
-      this.route.params.subscribe(params => {
-          this.claimId = params.id;
-      });
+    ngOnInit() {
+        this.route.params.subscribe(params => {
+            this.claimId = params.id;
+        });
 
-      this.claim = this.rewardClaimService.getClaim(this.claimId).subscribe(
+        this.claim = this.rewardClaimService.getClaim(this.claimId).subscribe(
 
-          result => console.log(this.claim),
-          err =>  console.log('Could not authenticate')
-      );
+            result => console.log(this.claim),
+            err => console.log('Could not authenticate')
+        );
 
-      console.log(this.claim);
-  }
+        console.log(this.claim);
+    }
 
     public submit() {
         this.rewardClaimService.updateClaimStatus(this.claimId, this.selectedValue);
-        this.router.navigate(['/rewardClaims'];
+        this.router.navigate(['/rewardClaims']);
     }
 
     public backLink() {
